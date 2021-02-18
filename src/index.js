71bcc289a3ab40fdc4dbae1e1edced2cae1451c5
Yors <jatbribiesca@gmail.com>
2021-02-18 11:33:35 -0600

    Create nav and set background

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Tooltip, Toast, Popover } from 'bootstrap';
+
+import navBar from './assets/js/navBar';
+import bgImage from './assets/images/mainBg.jpg'
+
+const background = () => {
+  document.body.style.background = `linear-gradient(rgba(0,0,0,0.8), rgba(0,0,0,0.8)), url(${bgImage}) no-repeat`;
+  document.body.style.backgroundSize = 'auto, cover';
+  document.body.style.backgroundRepeat = 'repeat';
+}
+
+document.addEventListener('DOMContentLoaded', () => { 
+  background()
+  navBar()
+})
